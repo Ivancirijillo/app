7cb9b33e756c85873b1112dfd54ed91b5600eca8
Ivan Tellez <2602its@gmail.com>
2023-03-21 15:55:40 -0600

    docs: :books: Mas consultas

--- Documentos/SQL/Doc_Consultas.sql
+++ Documentos/SQL/Doc_Consultas.sql
@@ -1,4 +1,3 @@
-
 select *from prueba;
 #Elias
 select  m.NombreM, p.SECCION, p.PRI, p.PAN, p.MORENA, p.PRD, p.IND, p.TOTAL_VOTOS, p.LISTA_NOMINAL  
@@ -8,18 +7,26 @@ on p.ClaveMunicipal = m.ClaveMunicipal
 order by p.ClaveMunicipal;
 
 #Marcos Delincuencia
-select *from Delincuencia where YearD='2022';
-
 select d.ClaveMunicipal, m.NombreM, d.YearD, d.DelitosAI, d.Homicidios, d.Feminicidios, d.Secuestros, d.DespT, (d.Robo + d.RoboT) as robtt
 from Delincuencia as d
 inner join Municipio as m 
 on d.ClaveMunicipal = m.ClaveMunicipal 
-where YearD='2022' and d.ClaveMunicipal='15001';
+where d.YearD='2022' and d.ClaveMunicipal='15001';
 
 #Marcos numero de habitantes
+select t.ClaveMunicipal, m.NombreM, t.YearP, t.Poblacion
+from TPobreza as t
+inner join Municipio as m 
+on t.ClaveMunicipal = m.ClaveMunicipal 
+where t.YearP='2020' and t.ClaveMunicipal='15001';
 
-
-
+#Marcos Pobreza PENDIENTE
 select *from TPobreza where YearP='2022';
+
+#Marcos Padron Electoral PENDIENTE
 select *from PadronElectoral where YearE='2022';
+
+#Marcos Apoyos PENDIENTE
 select *from Apoyos where YearA='2022';
+
+
