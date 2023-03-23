cc6d962d601f57b11200cd1a57b8798d9c39cdea
Ivan Tellez <2602its@gmail.com>
2023-03-23 15:11:13 -0600

    docs: :bookmark_tabs: Libreria FPDF

---
+++ static/pdf/conexion.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli = new mysqli("localhost","root","","basemunicipios");
+ 
+if ($mysqli->connect_errno)
+   header('Location: /');
+
+$mysqli->set_charset('utf8');
+
+return $mysqli;
+
