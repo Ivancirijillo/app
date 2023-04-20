1e49324286f8083d8d9eef7d4eebe6ca007c62af
Ivancirijillo <2602its@gmail.com>
2023-04-20 09:42:58 -0600

    fix: :bug: Boton

--- static/js/index.js
+++ static/js/index.js
@@ -29,13 +29,13 @@
 }*/
 
 //pantalla completa
-//     const elem = document.documentElement;
-// if (elem.requestFullscreen) {
-//     elem.requestFullscreen();
-//     } else if (elem.webkitRequestFullscreen) { /* Safari */
-//     elem.webkitRequestFullscreen();
-//     } else if (elem.msRequestFullscreen) { /* IE11 */
-//     elem.msRequestFullscreen();
+//      const elem = document.documentElement;
+//  if (elem.requestFullscreen) {
+//      elem.requestFullscreen();
+//      } else if (elem.webkitRequestFullscreen) { /* Safari */
+//      elem.webkitRequestFullscreen();
+//      } else if (elem.msRequestFullscreen) { /* IE11 */
+//      elem.msRequestFullscreen();
 // }
 
 //tipos
