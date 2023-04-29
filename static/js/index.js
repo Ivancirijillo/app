6efb28d5fac1416dc0edc6291d8bd6390b53e192
Malware543 <eliassalazar543@gmail.com>
2023-04-29 16:39:31 -0600

    refactor: :recycle: Eliminacion de codigo js
    Eliminacion de codigo basura en index

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,6 @@
-/*class Enviar{
-    constructor(ruta, metodo){
-        this.ruta = ruta;
-        this.metodo = metodo;
-        this.datos = ""
-    }
-
-    enviar_datos(datos) {
-        fetch(`${this.ruta}`, {
-            method: `${this.metodo}`,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(datos)
-            })
-            .then(response => response.json())
-            .then(data => {
-                this.set_datos(data);
-            });
-    }
-
-    get_datos(){
-        return `${this.datos.toString()}`;
-    }
-
-    set_datos(datos){
-        this.datos = datos;
-    }
-}*/
-
 //pantalla completa
-//      const elem = document.documentElement;
-//  if (elem.requestFullscreen) {
+// const elem = document.documentElement;
+// if (elem.requestFullscreen) {
 //      elem.requestFullscreen();
 //      } else if (elem.webkitRequestFullscreen) { /* Safari */
 //      elem.webkitRequestFullscreen();
@@ -38,50 +8,34 @@
 //      elem.msRequestFullscreen();
 // }
 
-//tipos
-const VARIOS = "varios";
-const RANGO = "rango";
-const NOMBRE = "nombre";
-const TODO = "todo";
-//expresiones
-const ID_MUNICIPIO = /^15(?:0[0-9][0-9]|1[0-2][0-5])(?:,(?!$)15(?:0[0-9][0-9]|1[0-2][0-5]))*(?:-15(?:0[0-9][0-9]|1[0-2][0-5]))?$/;
-const NOMBRE_MUNICIPIO = /^[a-zA-Z\s]{6,20}$/;
-const SECCION_MUNICIPIO = /^(?:[1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8])(?:,(?!$)([1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8]))*(?:-(?!$)([1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8]))?$/;
+//tipos de busqiedas
+const TIPOS = Object.freeze({
+    VARIOS:"varios",
+    RANGO:"rango",
+    NOMBRE:"nombre"
+});
+//expresiones regulares
+const EXPRESIONES = Object.freeze({
+    ID_MUNICIPIO:/^15(?:0[0-9][0-9]|1[0-2][0-5])(?:,(?!$)15(?:0[0-9][0-9]|1[0-2][0-5]))*(?:-15(?:0[0-9][0-9]|1[0-2][0-5]))?$/,
+    NOMBRE_MUNICIPIO:/^[a-zA-Z\s]{6,20}$/,
+    SECCION_MUNICIPIO:/^(?:[1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8])(?:,(?!$)([1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8]))*(?:-(?!$)([1-9]|[0-9][0-9]{1,2}|[0-5][0-9]{3}|6[0-4][0-9][0-9]|649[0-8]))?$/
+});
 //arregos
-const PARTIDOS= ["PAN","PRI", "PRD", "PT", "PVEM", "MC", "NA", "MORENA", "ES", "VR", "PH", "PES", "PFD", "RSP", "FXM", "NAEM", "INDEP"];
-const COLORES = ["#0453A5", "#FF0108","#FFB928", "#FD4146", "#00C65C", "#FF7400",   "#33BDBD", "#BA0005",  "#B632BF", "#FF018C", "#DC3892",    "#72017A", "#FF9945", "#FD4146", "#EF7CBB", "#6BDBDB", "#BB9A00" ];
-const COLORES2 = ['#0453A5','#FF7400','#FFB928','#FF0108'];
-const NUMEROS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const ARREGLOS = Object.freeze({
+    PARTIDOS:["PAN","PRI", "PRD", "PT", "PVEM", "MC", "NA", "MORENA", "ES", "VR", "PH", "PES", "PFD", "RSP", "FXM", "NAEM", "INDEP"],
+    COLORES:["#0453A5", "#FF0108","#FFB928", "#FD4146", "#00C65C", "#FF7400",   "#33BDBD", "#BA0005",  "#B632BF", "#FF018C", "#DC3892",    "#72017A", "#FF9945", "#FD4146", "#EF7CBB", "#6BDBDB", "#BB9A00" ],
+    COLORES2:['#0453A5','#FF7400','#FFB928','#FF0108'],
+});
+
+// const PARTIDOS = ["PAN","PRI", "PRD", "PT", "PVEM", "MC", "NA", "MORENA", "ES", "VR", "PH", "PES", "PFD", "RSP", "FXM", "NAEM", "INDEP"];
+// const COLORES = ["#0453A5", "#FF0108","#FFB928", "#FD4146", "#00C65C", "#FF7400",   "#33BDBD", "#BA0005",  "#B632BF", "#FF018C", "#DC3892",    "#72017A", "#FF9945", "#FD4146", "#EF7CBB", "#6BDBDB", "#BB9A00" ];
+//const COLORES2 = ['#0453A5','#FF7400','#FFB928','#FF0108'];
 
 //let boton = document.querySelector(".buscar")
 let buscador = document.querySelector(".Ibuscar")
 let boton_buscador = document.querySelector(".Bbuscar")
 let botones_rapidos = document.querySelectorAll(".sRapida")
 boton_buscador.disabled = true
-
-// boton.addEventListener("click",(e)=>{
-//     e.preventDefault()
-//     let municipios = document.querySelectorAll(".municipio")
-//     let municipioP = [];
-    
-//     ventana_carga();
-    
-//     municipios.forEach(municipio => {
-//         if (municipio.checked){ 
-//             municipioP.push(municipio.getAttribute("value"))
-//         }
-//     });
-
-//     json = {
-//         tipo: NOMBRE,
-//         datos: municipioP[0],
-//         years: [2015,2017,2018,2021]
-//     }
-//     enviar_datos(json)
-//     .then(data=>{
-//         crear_grafica(data, NOMBRE);
-//     })
-// });
 
 //FUNCION DEL BOTON MOSTRAR Y OCULTAR
 const contenedordiv = document.querySelector('#mostrar')
@@ -148,7 +102,7 @@ botones_rapidos.forEach(element => {
 buscador.addEventListener("input",(e)=>{
     e.preventDefault();
     let dato = buscador.value
-    if(ID_MUNICIPIO.test(dato) || NOMBRE_MUNICIPIO.test(dato) || SECCION_MUNICIPIO.test(dato)) { 
+    if(EXPRESIONES.ID_MUNICIPIO.test(dato) || EXPRESIONES.NOMBRE_MUNICIPIO.test(dato) || EXPRESIONES.SECCION_MUNICIPIO.test(dato)) { 
         boton_buscador.disabled = false;
         boton_buscador.style.borderColor = "#0453A5";
     }else {
@@ -160,7 +114,7 @@ buscador.addEventListener("input",(e)=>{
 buscador.addEventListener("blur",(e)=>{
     e.preventDefault()
     let dato = buscador.value;
-    if(ID_MUNICIPIO.test(dato) || NOMBRE_MUNICIPIO.test(dato) || SECCION_MUNICIPIO.test(dato)) { 
+    if(EXPRESIONES.ID_MUNICIPIO.test(dato) || EXPRESIONES.NOMBRE_MUNICIPIO.test(dato) || EXPRESIONES.SECCION_MUNICIPIO.test(dato)) { 
         boton_buscador.disabled = false;
         boton_buscador.style.borderColor = "#0453A5";
     }else {
@@ -223,130 +177,64 @@ function analizar_datos(){
     let json = {};
 
     if(datos.indexOf(",") != -1){
-        tipo = VARIOS;
+        tipo = TIPOS.VARIOS;
+        datos_analizados =  datos.split(",");
+        json = {
+            tipo: tipo,
+            datos: datos_analizados,
+            years:obtener_years()
+        }
+        enviar_datos(json)
+        .then(data_s => {
+            //console.log(data_s.datos);
+            crear_grafica(data_s);
+        });
+
     } else if(datos.indexOf("-") != -1){
-        tipo = RANGO;
-    } else tipo = NOMBRE;
-    
-    switch(tipo){
-        case VARIOS:
-            datos_analizados = datos.split(",");
-            json = {
-                tipo: VARIOS,
-                datos: datos_analizados,
-                years:obtener_years()
-            }
-            enviar_datos(json)
-            .then(data_s => {
-                console.log(data_s.datos);
-                crear_grafica(data_s, tipo);
-            });
-            break;
-        case RANGO:
-            datos_analizados = datos.split("-");
-            json = {
-                tipo: RANGO,
-                datos: datos_analizados,
-                years: obtener_years()
-            }
-            enviar_datos(json)
-            .then(data_s => {
-                console.log(data_s.datos);
-                crear_grafica(data_s, tipo);
-            });
-            break;
-        case NOMBRE:
-            let listayear = obtener_years();
-            json = {
-                tipo: NOMBRE,
-                datos: datos,
-                years:listayear
-            }
-            console.log(json);
-            enviar_datos(json)
-            .then(data_s => {
-                //console.log(Object.keys(data_s.datos));
-                crear_grafica(data_s, tipo);
-            });
-            break;
+        tipo = TIPOS.RANGO;
+        datos_analizados =  datos.split("-");
+        json = {
+            tipo: tipo,
+            datos: datos_analizados,
+            years:obtener_years()
+        }
+        enviar_datos(json)
+        .then(data_s => {
+            //console.log(data_s.datos);
+            crear_grafica(data_s);
+        });
+    } else { 
+        tipo = TIPOS.NOMBRE
+        json = {
+            tipo: tipo,
+            datos: datos,
+            years:obtener_years()
+        }
+        enviar_datos(json)
+        .then(data_s => {
+            //console.log(data_s.datos);
+            crear_grafica(data_s);
+        });
     }
 }
 
-function crear_grafica(data_s, tipo){
+function crear_grafica(data_s){
 
     let municipios = [];
     let years = Object.keys(data_s.datos);
     let partidos = [];
     let chartData = {};
 
-    if(tipo == VARIOS){
-        municipios = encontrar_municipios(years, data_s);
-        partidos = ordenar_partidos(municipios, years, data_s);
-        chartData = crear_diccionario(municipios, years, partidos)
-    }else if(tipo == RANGO){
-        municipios = encontrar_municipios(years, data_s);
-        //console.log(municipios[0].length);
-        //let partido = data_s.datos[years[i]][municipios[1]][PARTIDOS[0]]
-        //console.log(municipios[0])
-        //console.log(Object.keys(data_s.datos[years[0]][municipios[0][0]]));
-        partidos = ordenar_partidos(municipios, years, data_s);
-        console.log(partidos);
-
-        chartData = crear_diccionario(municipios, years, partidos);
-        console.log(chartData);
-        // let municipio = Object.keys(data_s.datos)[0];
-        // let contieneNumero = NUMEROS.some(numero => municipio.includes(numero));
-        // if(contieneNumero){
-        //     municipios = Object.keys(data_s.datos)
-        //     municipios.forEach((item)=>{
-        //         lista_partidos.push(data_s.datos[item]);
-        //     });
-        //     console.log(lista_partidos);
-        // } else{
-        //     for(let i = 0 ;i<Object.keys(data_s.datos).length;i++){
-        //         partidos.push(Object.keys(data_s.datos[`${Object.keys(data_s.datos)[i]}`]));
-        //         votos.push([])
-        //         for (let j = 0; j < PARTIDOS.length; j++) {
-        //             votos[i].push(data_s.datos[`${Object.keys(data_s.datos)[i]}`][PARTIDOS[j]]);
-        //         } 
-        //     }
-        //     let aux = 0;
-        //     while(partidos.length > aux){
-        //         lista_partidos.push([]);
-        //         for(let i = 0;i<PARTIDOS.length;i++){
-        //             let data = (votos[aux][i].reduce((total, num)=>total+num,0));
-        //             lista_partidos[aux].push(data)
-        //         }
-        //         aux++;
-        //     }
-        //     console.log(municipios[0]);
-        //     console.log(lista_partidos);
-        // }
-
-    } else if(tipo == NOMBRE){
-        //console.log(Object.keys(data_s));
-        municipios = encontrar_municipios(years, data_s);
-        //console.log(municipios);
-
-        partidos = ordenar_partidos(municipios, years, data_s);
-        //console.log(partidos);
-
-        chartData = crear_diccionario(municipios, years, partidos);
-        //console.log(chartData["ACAMBAY DE RUÍZ CASTAÑEDA. SECCION:1"]);
-    }
-    
+    municipios = encontrar_municipios(years, data_s);
+    partidos = ordenar_partidos(municipios, years, data_s);
+    chartData = crear_diccionario(municipios, years, partidos);
 
     let fragmento = document.createDocumentFragment();
     let municipio = [];
     for(let i=0;i<municipios.length;i++){
-        municipio.splice();
-        // for (let j = 0; j < 6; j++) {
-        //     if(chartData[j].label.includes(municipios[i])){
-        //         municipio.push(chartData[j]);
-        //         //console.log(municipios[i]);
-        //     }
-        // }
-        municipio = chartData.filter(item => item.label.includes(municipios[i]));
+        //municipio.splice();
+        // verificar el filtrado, incongruncias al encontrar municipios con nombre parecido.
+        municipio = chartData.filter(item => item.label.includes(municipios[i])); 
         console.log(municipio);
         let canvas = document.createElement("canvas");
         canvas.setAttribute("class", "grafica0");
@@ -355,7 +243,7 @@ function crear_grafica(data_s, tipo){
         let char = new Chart(contexto, {
                 type: "bar",
                 data: {
-                labels: PARTIDOS,
+                labels: ARREGLOS.PARTIDOS,
                 datasets: municipio
                 }
             });
@@ -370,19 +258,27 @@ function crear_grafica(data_s, tipo){
 
 function obtener_years(){
     let years = document.querySelectorAll(".cbox");
-    let listayear = [];
-    years.forEach(item=>{
-        if(item.checked){
-            listayear.push(item.getAttribute("value"));
-        }
-    });
+    let listayear = Array.from(years).reduce((year, item)=>{
+        if (item.checked) year.push(item.getAttribute("value"));
+        return year;
+    },[]);
+    
     return listayear;
 }
 
 function encontrar_municipios(years, data_s){
     let municipios = [];
     let n_municipios = Object.keys(data_s.datos[years[0]]).length
-    //console.log(n_municipios)
+    /**formato de js(data_s)
+     * 1.data_s.datos[years[0]] determina el año
+     * 2.data_s.datos[years[0]][2] determina el la posicion del arreglo
+     * 3.data_s.datos[years[0]][2]["ACULCO"] determina el municipio
+     * 4.data_s.datos[years[0]][2]["ACULCO"]["PAN"] determina el partido
+     * nota: el 2 y el 3 deben ser iguales, ejemplo:
+     * si existen 3 municipios 2 debe de estar posicionado en el arreglo donde
+     * se encuentre aculco, es el numero del municipio.
+     */
+    //console.log(data_s.datos[years[0]][0][municipios[0]][PARTIDOS[0]]);
 
     for (let i = 0; i < years.length; i++) {
         for (let j = 0; j < n_municipios; j++) {
@@ -392,29 +288,7 @@ function encontrar_municipios(years, data_s){
             }
         }
     }
-    
-    // years.forEach((item)=>{
-    //     let estaEnLista = municipios.some(municipio=>municipio.includes(municipio));
-    //     if (estaEnLista){} else municipios.push(Object.keys(data_s.datos[item]));
-    // });
-
-    // years.forEach((item)=>{
-    //     let estaEnLista = municipios.some(municipio => municipios.includes(municipio));
-    //     if (estaEnLista){} else municipios.push(Object.keys(data_s.datos[item]));
-    // });
         
-    /**formato de js
-     * 1.data_s.datos[years[0]] determina el año
-     * 2.data_s.datos[years[0]][2] determina el la posicion del arreglo
-     * 3.data_s.datos[years[0]][2]["ACULCO"] determina el municipio
-     * 4.data_s.datos[years[0]][2]["ACULCO"]["PAN"] determina el partido
-     * nota: el 2 y el 3 deben ser iguales, ejemplo:
-     * si existen 3 municipios 2 debe de estar posicionado en el arreglo donde
-     * se encuentre aculco, es el numero del municipio.
-     */
-    //console.log(municipios)
-    //console.log(data_s.datos[years[0]][0][municipios[0]][PARTIDOS[0]]);
-    
     return municipios;
 }
 
@@ -427,13 +301,13 @@ function ordenar_partidos(municipios, years, data_s){
         for (let i = 0; i < years.length; i++) {
             //console.log(i)
             let partidosAnuales = [];
-            for (let j = 0; j < PARTIDOS.length; j++) {
+            for (let j = 0; j < ARREGLOS.PARTIDOS.length; j++) {
                 //console.log(`${PARTIDOS[j]}:`+data_s.datos[years[i]][aux][municipios[aux]][PARTIDOS[j]])
                 try{
                     //let partido = data_s.datos[years[i]][aux][municipios[aux]][PARTIDOS[j]];
-                    if(data_s.datos[years[i]][aux][municipios[aux]][PARTIDOS[j]] === undefined){
+                    if(data_s.datos[years[i]][aux][municipios[aux]][ARREGLOS.PARTIDOS[j]] === undefined){
                         partidosAnuales.push(0);
-                    }else partidosAnuales.push(data_s.datos[years[i]][aux][municipios[aux]][PARTIDOS[j]]);
+                    }else partidosAnuales.push(data_s.datos[years[i]][aux][municipios[aux]][ARREGLOS.PARTIDOS[j]]);
                 }catch(error){
                     console.log(error);
                 }
@@ -454,6 +328,12 @@ function crear_diccionario(municipios, years, partidos){
     let diccionario = {};
     let chartData = [];
     let aux = 0;
+
+    //formato diccionario
+    //diccionario[0] indica el año
+    //diccionario[0][0] indica el municipio
+    //let info = diccionario[0][0]
+
     for (let year in years) {
         diccionario[year] = [];
         for (let j = 0; j < municipios.length; j++) {
@@ -466,7 +346,7 @@ function crear_diccionario(municipios, years, partidos){
             diccionario[j][i]={
                 label: `${municipios[i]} año ${years[j]}`,
                 data: partidos[aux],
-                backgroundColor: COLORES[j],
+                backgroundColor: ARREGLOS.COLORES[j],
                 borderColor: "rgba(0,99,132,1)",
                 yAxisID: "y-axis-destiny"
             }
@@ -488,24 +368,8 @@ function crear_diccionario(municipios, years, partidos){
 
             chartData.push(tempData);
         }
-        
     }
-
-    let info = diccionario[0][0]
-    //console.log(chartData[5].label.includes(municipios[0]));
     return chartData;
-    //console.log(Object.values(info.data));
-    //formato diccionario
-    //diccionario[] indica el año
-    //diccionario[][] indica el municipio
-    // for (let i = 0; i < years.length; i++) {
-    //     for (let j = 0; j < municipios.length; j++) {
-    //         console.logObject.keys((diccionario[i][j]));
-    //     }
-        
-    // }
-
-    
 }
 
 function scrollToSection(sectionId) {
