cf8ed305ef3336942bf0f0f83ee445ceb52f09d6
Ivancirijillo <2602its@gmail.com>
2023-05-21 14:50:56 -0600

    style: :art: Variables para estilos

--- static/js/borrador.js
+++ static/js/borrador.js
@@ -1,3 +1,4 @@
+const vivienda =['Viviendas con piso de tierra', 'Viviendas que no disponen de excusado o sanitario', 'Viviendas que no disponen de agua entubada de la red pública', 'Viviendas que no disponen de drenaje', 'Viviendas que no disponen de energía eléctrica', 'viviendas que no disponen de lavadora', 'Viviendas que no disponen de refrigerador']
 
 function autoScroll(sectionId) {
     const section = document.getElementById(sectionId);
@@ -7,3 +8,28 @@ function autoScroll(sectionId) {
         behavior: 'smooth'
     });
 }
+
+const ctx = document.getElementById('GReSo');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Viviendas con piso de tierra', 'Viviendas que no disponen de excusado o sanitario', 'Viviendas que no disponen de agua entubada de la red pública', 'Viviendas que no disponen de drenaje', 'Viviendas que no disponen de energía eléctrica', 'viviendas que no disponen de lavadora', 'Viviendas que no disponen de refrigerador'],
+      datasets: [{
+        label: 'Número de viviendas',
+        data: [3.8, 14.9, 10.4, 20.9, 2.6, 63.2, 32.4],
+
+        borderWidth: 1
+      }]
+    },
+    options: {
+        indexAxis: 'y',
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        responsive: true,
+        
+    }
+  });
