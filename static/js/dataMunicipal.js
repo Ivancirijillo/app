088db9e65401bb498ed42e684d9c3d0ad47dcaea
Lucero Antonio <luc.an.mart@gmail.com>
2023-07-18 10:19:07 -0600

    estilos tooltip

--- static/js/dataMunicipal.js
+++ static/js/dataMunicipal.js
@@ -148,6 +148,22 @@ function graficaBa(seccionID, etiquetas, etiquetasDatasets, datos) {
           beginAtZero: true
         }
       },
+      interaction: {
+        intersect: true,
+        mode: 'index',
+      },
+      plugins: {
+        subtitle: {
+          display: true,
+          text: 'Dar clic sobre el año para deseleccionar o seleccionar',
+          color: '#00817d',
+        },
+        
+        tooltip: {
+          enabled: false,
+          external: externalTooltipHandler
+        }
+      },
       responsive: true,
     }
   });
@@ -173,6 +189,16 @@ function graficaLi (seccionID, etiqueta, datos, etiquetas ) {
       labels: {
         boxWidth: 80,
         fontColor: 'black'
+      }
+    },
+    interaction: {
+      intersect: false,
+      mode: 'index',
+    },
+    plugins: {
+      tooltip: {
+        enabled: false,
+        external: externalTooltipHandler
       }
     }
 };
@@ -460,3 +486,115 @@ function consultaT(dato) {
       .catch(error => reject(error));
   });
 }
+
+
+/// configuracion para el tooltip
+
+const getOrCreateTooltip = (chart) => {
+  let tooltipEl = chart.canvas.parentNode.querySelector('div');
+
+  // Estilos del tooltip
+  if (!tooltipEl) {
+    tooltipEl = document.createElement('div');
+    tooltipEl.style.fontSize = '13px';
+    tooltipEl.style.background = 'rgba(0, 0, 0,0.7)';
+    tooltipEl.style.borderRadius = '10px';
+    tooltipEl.style.color = 'black';
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.pointerEvents = 'none';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.transform = 'translate(-50%, 0)';
+    tooltipEl.style.transition = 'all .1s ease';
+
+    const table = document.createElement('table');
+    table.style.margin = '0px';
+
+    tooltipEl.appendChild(table);
+    chart.canvas.parentNode.appendChild(tooltipEl);
+  }
+
+  return tooltipEl;
+};
+
+
+const externalTooltipHandler = (context) => {
+  // Tooltip Element
+  const {chart, tooltip} = context;
+  const tooltipEl = getOrCreateTooltip(chart);
+
+  // Hide if no tooltip
+  if (tooltip.opacity === 0) {
+    tooltipEl.style.opacity = 0;
+    return;
+  }
+
+  
+
+  // Set Text
+  if (tooltip.body) {
+    const titleLines = tooltip.title || [];
+    const bodyLines = tooltip.body.map(b => b.lines);
+
+    const tableHead = document.createElement('thead');
+
+    titleLines.forEach(title => {
+      const tr = document.createElement('tr');
+      tr.style.borderWidth = 0;
+
+      const th = document.createElement('th');
+      th.style.borderWidth = 0;
+      th.style.backgroundColor = '#00afaa';
+      const text = document.createTextNode(title);
+
+      th.appendChild(text);
+      tr.appendChild(th);
+      tableHead.appendChild(tr);
+    });
+
+    const tableBody = document.createElement('tbody');
+    bodyLines.forEach((body, i) => {
+      const colors = tooltip.labelColors[i];
+
+      const span = document.createElement('span');
+      span.style.background = colors.backgroundColor;
+      span.style.borderColor = colors.borderColor;
+      span.style.borderWidth = '1px';
+      span.style.marginRight = '10px';
+      span.style.height = '8px';
+      span.style.width = '10px';
+      span.style.display = 'inline-block';
+
+      const tr = document.createElement('tr');
+      tr.style.backgroundColor = '#ccc';
+      tr.style.borderWidth = 0;
+
+      const td = document.createElement('td');
+      td.style.borderWidth = 0;
+
+      const text = document.createTextNode(body);
+
+      td.appendChild(span);
+      td.appendChild(text);
+      tr.appendChild(td);
+      tableBody.appendChild(tr);
+    });
+
+    const tableRoot = tooltipEl.querySelector('table');
+
+    // Remove old children
+    while (tableRoot.firstChild) {
+      tableRoot.firstChild.remove();
+    }
+
+    // Add new children
+    tableRoot.appendChild(tableHead);
+    tableRoot.appendChild(tableBody);
+  }
+
+  const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
+
+  // Display, position, and set styles for font
+  tooltipEl.style.opacity = 1;
+  tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+  tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+};
